a40da1991b803973a14de0848088da9a226cea30
t-namitc <t-namitc@microsoft.com>
2014-06-12 12:57:35 -0700

    Synced my files
    Yeah!

---
+++ ViRT/FetchCustomer.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+public partial class FetchCustomer : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string st = ConfigurationManager.ConnectionStrings["old"].ConnectionString;
+        var id = Request.QueryString["Tag"];
+        if (id != null)
+        {
+            using (SqlConnection con = new SqlConnection(st))
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT Top 1000000 * FROM [dbo].[ProdDollar_TagAggregation] where Tag='" +id+"'", con))
+                {
+                    con.Open();
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    if (rdr.HasRows)
+                    {
+                        while (rdr.Read())
+                        {
+                            Response.Write("<p>");
+                            Response.Write("<strong> Tag:" + rdr["Tag"].ToString() + "</strong><br />");
+                            Response.Write("<strong> NetworkID:" + rdr["NetworkID"].ToString() + "</strong><br />");
+                            Response.Write("<strong> FarmID:" + rdr["FarmID"].ToString() + "</strong><br />");
+                            Response.Write("<strong> Date:" + rdr["Date"].ToString() + "</strong><br />");
+                            Response.Write("</p>");
+
+                        }
+                    }
+                }
+            }
+        }
+        else
+        {
+        }
+    }
+}
--- ViRT/Properties/AssemblyInfo.cs
+++ ViRT/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ViRT")]
+[assembly: AssemblyTitle("WebApplication1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Hewlett-Packard Company")]
-[assembly: AssemblyProduct("ViRT")]
-[assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2014")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebApplication1")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("49c8bd1f-9155-4114-ae6d-bd3a23818c1a")]
+[assembly: Guid("ea57a6e4-9432-42c5-b5d5-c6d63e1f1539")]
 
 // Version information for an assembly consists of the following four values:
 //
