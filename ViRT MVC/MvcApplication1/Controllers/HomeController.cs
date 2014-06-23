3ba589c6aae6863763a228f68d116e8e9bf89039
t-fitrej <t-fitrej@microsoft.com>
2014-06-23 10:57:35 -0700

    Fixed DataCenter Bug in Reliability

--- ViRT MVC/MvcApplication1/Controllers/HomeController.cs
+++ ViRT MVC/MvcApplication1/Controllers/HomeController.cs
@@ -7,6 +7,8 @@ using System.Web.SessionState;
 using System.Data;
 using MvcApplication1.Models;
 using Newtonsoft.Json;
+using System.Data.SqlClient;
+
 
 
 namespace MvcApplication1.Controllers
@@ -16,14 +18,32 @@ namespace MvcApplication1.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
-			
 			Reliability world = new Reliability();
 
 			DataTable worldLocs = world.GetDataCenterLatLong();
-
+			world.ChangeDate((new DateTime(2014, 06, 18)), new DateTime(2014, 06, 19));
+			world.ChangePipeline("DynamicClaims");
 			var json = JsonConvert.SerializeObject(worldLocs);
+			String[] dcs = world.GetAllDataCentersArray();
 
+			DataTable dcPipeAverage = new DataTable();
+			dcPipeAverage.Columns.Add("DataCenter", typeof(string));
+			dcPipeAverage.Columns.Add("Percent", typeof(decimal));
+
+			DataRow temp = dcPipeAverage.NewRow();
+
+			for (int i = 0; i < dcs.Length; i++)
+			{
+				world.ChangeDataCenter(dcs[i]);
+				temp["DataCenter"] = dcs[i];
+				temp["Percent"] = world.CalculatePipeOverview();
+				dcPipeAverage.Rows.Add(temp);
+				temp = dcPipeAverage.NewRow();
+			}
+
+			var percentages = JsonConvert.SerializeObject(dcPipeAverage);
+
+			ViewBag.AverageDCPercent = percentages;
 			ViewBag.WorldMap = json;
             return View();
         }
