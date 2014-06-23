95e2f75eae197cb7ade262ba33023bfbde1d3305
t-fitrej <t-fitrej@microsoft.com>
2014-06-23 15:28:42 -0700

    Fixed stuff?

--- ViRT MVC/MvcApplication1/Models/ReliabilityModels.cs
+++ ViRT MVC/MvcApplication1/Models/ReliabilityModels.cs
@@ -726,7 +726,6 @@ namespace MvcApplication1.Models
             {
                 DataRow newRow = farms.NewRow();
 				newRow["FarmID"] = random.Next(1000, 10000);
-<<<<<<< HEAD
 				newRow["Percentage"] = Convert.ToDouble(String.Format("{0:0.0000}", random.NextDouble() * 100));
 				farms.Rows.Add(newRow);
 			}
@@ -738,12 +737,5 @@ namespace MvcApplication1.Models
 			return pipeline;
 		}
 	}
-=======
-                newRow["Percentage"] = Convert.ToDouble(String.Format("{0:0.0000}", random.NextDouble() * 100));
-                farms.Rows.Add(newRow);
-            }
-            return farms;
-        }
-    }
->>>>>>> origin/master
+
 }
